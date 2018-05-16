92df6367d4e4154a8dbbc55dce66bf6f2e705ca8
chen <844386320@qq.com>
2018-05-16 21:51:28 +0800

    change model struct(kernel num)

--- FCN_Atrous.py
+++ FCN_Atrous.py
@@ -30,128 +30,83 @@ def FCN8_Atrous( nClasses ,  input_height=416, input_width=608 , vgg_level=3):
 	# https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_th_dim_ordering_th_kernels.h5
 	img_input = Input(shape=(3,input_height,input_width))
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(img_input)
-	x = Conv2D(16, (3, 3), activation='relu', name='block1_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(8, (3, 3), activation='relu', name='block1_conv1', data_format=IMAGE_ORDERING )(x)
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
-	x = Conv2D(16, (3, 3), activation='relu', name='block1_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(8, (3, 3), activation='relu', name='block1_conv2', data_format=IMAGE_ORDERING )(x)
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(img_input)
 	x = MaxPooling2D((3, 3), strides=(2, 2), name='block1_pool', data_format=IMAGE_ORDERING )(x)
 
 	# Block 2
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
-	x = Conv2D(32, (3, 3), activation='relu', name='block2_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(16, (3, 3), activation='relu', name='block2_conv1', data_format=IMAGE_ORDERING )(x)
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
-	x = Conv2D(32, (3, 3), activation='relu', name='block2_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(16, (3, 3), activation='relu', name='block2_conv2', data_format=IMAGE_ORDERING )(x)
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
 	x = MaxPooling2D((3, 3), strides=(2, 2), name='block2_pool', data_format=IMAGE_ORDERING )(x)
 	f2 = x
-
+	
 	# Block 3
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
-	x = Conv2D(64, (3, 3), activation='relu', name='block3_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(32, (3, 3), activation='relu', name='block3_conv1', data_format=IMAGE_ORDERING )(x)
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
-	x = Conv2D(64, (3, 3), activation='relu', name='block3_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(32, (3, 3), activation='relu', name='block3_conv2', data_format=IMAGE_ORDERING )(x)
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
-	x = Conv2D(64, (3, 3), activation='relu', name='block3_conv3', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(32, (3, 3), activation='relu', name='block3_conv3', data_format=IMAGE_ORDERING )(x)
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
 	x = MaxPooling2D((3, 3), strides=(2, 2), name='block3_pool', data_format=IMAGE_ORDERING )(x)
 	f3 = x
 
 	# Block 4
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
-	x = Conv2D(128, (3, 3), activation='relu', name='block4_conv1', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(64, (3, 3), activation='relu', name='block4_conv1', data_format=IMAGE_ORDERING )(x)
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
-	x = Conv2D(128, (3, 3), activation='relu', name='block4_conv2', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(64, (3, 3), activation='relu', name='block4_conv2', data_format=IMAGE_ORDERING )(x)
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
-	x = Conv2D(128, (3, 3), activation='relu', name='block4_conv3', data_format=IMAGE_ORDERING )(x)
+	x = Conv2D(64, (3, 3), activation='relu', name='block4_conv3', data_format=IMAGE_ORDERING )(x)
 	x = ZeroPadding2D(padding=(1,1), data_format=IMAGE_ORDERING )(x)
 	h = MaxPooling2D((3, 3), strides=(1, 1), name='block4_pool', data_format=IMAGE_ORDERING )(x)
 	f4 = x
-
-	'''
-	# Block 5
-	h = ZeroPadding2D(padding=(2, 2))(h)
-	h = AtrousConvolution2D(512, 3, 3, atrous_rate=(2, 2), activation='relu', name='conv5_1', data_format=IMAGE_ORDERING)(h)
-	h = ZeroPadding2D(padding=(2, 2))(h)
-	h = AtrousConvolution2D(512, 3, 3, atrous_rate=(2, 2), activation='relu', name='conv5_2', data_format=IMAGE_ORDERING)(h)
-	h = ZeroPadding2D(padding=(2, 2))(h)
-	h = AtrousConvolution2D(512, 3, 3, atrous_rate=(2, 2), activation='relu', name='conv5_3', data_format=IMAGE_ORDERING)(h)
-	h = ZeroPadding2D(padding=(1, 1))(h)
-	p5 = MaxPooling2D(pool_size=(3, 3), strides=(1, 1))(h)
-
-	# branching for Atrous Spatial Pyramid Pooling
-	# hole = 6
-	b1 = ZeroPadding2D(padding=(6, 6))(p5)
-	b1 = AtrousConvolution2D(1024, 3, 3, atrous_rate=(6, 6), activation='relu', name='fc6_1', data_format=IMAGE_ORDERING)(b1)
-	b1 = Dropout(0.5)(b1)
-	b1 = Convolution2D(1024, 1, 1, activation='relu', name='fc7_1', data_format=IMAGE_ORDERING)(b1)
-	b1 = Dropout(0.5)(b1)
-	b1 = Convolution2D(2, 1, 1, activation='relu', name='fc8_voc12_1', data_format=IMAGE_ORDERING)(b1)
-
-	# hole = 12
-	b2 = ZeroPadding2D(padding=(12, 12))(p5)
-	b2 = AtrousConvolution2D(1024, 3, 3, atrous_rate=(12, 12), activation='relu', name='fc6_2', data_format=IMAGE_ORDERING)(b2)
-	b2 = Dropout(0.5)(b2)
-	b2 = Convolution2D(1024, 1, 1, activation='relu', name='fc7_2', data_format=IMAGE_ORDERING)(b2)
-	b2 = Dropout(0.5)(b2)
-	b2 = Convolution2D(2, 1, 1, activation='relu', name='fc8_voc12_2', data_format=IMAGE_ORDERING)(b2)
-
-	# hole = 18
-	b3 = ZeroPadding2D(padding=(18, 18))(p5)
-	b3 = AtrousConvolution2D(1024, 3, 3, atrous_rate=(18, 18), activation='relu', name='fc6_3', data_format=IMAGE_ORDERING)(b3)
-	b3 = Dropout(0.5)(b3)
-	b3 = Convolution2D(1024, 1, 1, activation='relu', name='fc7_3', data_format=IMAGE_ORDERING)(b3)
-	b3 = Dropout(0.5)(b3)
-	b3 = Convolution2D(2, 1, 1, activation='relu', name='fc8_voc12_3', data_format=IMAGE_ORDERING)(b3)
-
-	# hole = 24
-	b4 = ZeroPadding2D(padding=(24, 24))(p5)
-	b4 = AtrousConvolution2D(1024, 3, 3, atrous_rate=(24, 24), activation='relu', name='fc6_4', data_format=IMAGE_ORDERING)(b4)
-	b4 = Dropout(0.5)(b4)
-	b4 = Convolution2D(1024, 1, 1, activation='relu', name='fc7_4', data_format=IMAGE_ORDERING)(b4)
-	b4 = Dropout(0.5)(b4)
-	b4 = Convolution2D(2, 1, 1, activation='relu', name='fc8_voc12_4', data_format=IMAGE_ORDERING)(b4)
-
-	'''
+	
 	# Block 5
 	h = ZeroPadding2D(padding=(2, 2), data_format=IMAGE_ORDERING)(h)
-	h = Conv2D(128, (3, 3),dilation_rate=(2, 2), activation='relu', name='conv5_1', data_format=IMAGE_ORDERING)(h)
+	h = Conv2D(64, (3, 3),dilation_rate=(2, 2), activation='relu', name='conv5_1', data_format=IMAGE_ORDERING)(h)
 	h = ZeroPadding2D(padding=(2, 2), data_format=IMAGE_ORDERING)(h)
-	h = Conv2D(128, (3, 3),dilation_rate=(2, 2), activation='relu', name='conv5_2', data_format=IMAGE_ORDERING)(h)
+	h = Conv2D(64, (3, 3),dilation_rate=(2, 2), activation='relu', name='conv5_2', data_format=IMAGE_ORDERING)(h)
 	h = ZeroPadding2D(padding=(2, 2), data_format=IMAGE_ORDERING)(h)
-	h = Conv2D(128, (3, 3),dilation_rate=(2, 2), activation='relu', name='conv5_3', data_format=IMAGE_ORDERING)(h)
+	h = Conv2D(64, (3, 3),dilation_rate=(2, 2), activation='relu', name='conv5_3', data_format=IMAGE_ORDERING)(h)
 	h = ZeroPadding2D(padding=(1, 1), data_format=IMAGE_ORDERING)(h)
 	p5 = MaxPooling2D(pool_size=(3, 3),strides=(1, 1), data_format=IMAGE_ORDERING)(h)
 
 	# branching for Atrous Spatial Pyramid Pooling
 	# hole = 6
 	# b1 = ZeroPadding2D(padding=(6, 6))(p5)
-	b1 = Conv2D(64, (3, 3), padding='same',dilation_rate=(6, 6), activation='relu', name='fc6_1', data_format=IMAGE_ORDERING)(p5)
+	b1 = Conv2D(10, (3, 3), padding='same',dilation_rate=(6, 6), activation='relu', name='fc6_1', data_format=IMAGE_ORDERING)(p5)
 	b1 = Dropout(0.5)(b1)
-	b1 = Conv2D(64, (1, 1), activation='relu', name='fc7_1', data_format=IMAGE_ORDERING)(b1)
+	b1 = Conv2D(10, (1, 1), activation='relu', name='fc7_1', data_format=IMAGE_ORDERING)(b1)
 	b1 = Dropout(0.5)(b1)
 	b1 = Conv2D(2, (1, 1), activation='relu', name='fc8_voc12_1', data_format=IMAGE_ORDERING)(b1)
 
 	# hole = 12
 	# b2 = ZeroPadding2D(padding=(12, 12))(p5)
-	b2 = Conv2D(64, (3, 3), padding='same',dilation_rate=(12, 12), activation='relu', name='fc6_2', data_format=IMAGE_ORDERING)(p5)
+	b2 = Conv2D(10, (3, 3), padding='same',dilation_rate=(12, 12), activation='relu', name='fc6_2', data_format=IMAGE_ORDERING)(p5)
 	b2 = Dropout(0.5)(b2)
-	b2 = Conv2D(64, (1, 1), activation='relu', name='fc7_2', data_format=IMAGE_ORDERING)(b2)
+	b2 = Conv2D(10, (1, 1), activation='relu', name='fc7_2', data_format=IMAGE_ORDERING)(b2)
 	b2 = Dropout(0.5)(b2)
 	b2 = Conv2D(2, (1, 1), activation='relu', name='fc8_voc12_2', data_format=IMAGE_ORDERING)(b2)
 
 	# hole = 18
 	# b3 = ZeroPadding2D(padding=(18, 18))(p5)
-	b3 = Conv2D(64, (3, 3), padding='same',dilation_rate=(18, 18), activation='relu', name='fc6_3', data_format=IMAGE_ORDERING)(p5)
+	b3 = Conv2D(10, (3, 3), padding='same',dilation_rate=(18, 18), activation='relu', name='fc6_3', data_format=IMAGE_ORDERING)(p5)
 	b3 = Dropout(0.5)(b3)
-	b3 = Conv2D(64, (1, 1), activation='relu', name='fc7_3', data_format=IMAGE_ORDERING)(b3)
+	b3 = Conv2D(10, (1, 1), activation='relu', name='fc7_3', data_format=IMAGE_ORDERING)(b3)
 	b3 = Dropout(0.5)(b3)
 	b3 = Conv2D(2, (1, 1), activation='relu', name='fc8_voc12_3', data_format=IMAGE_ORDERING)(b3)
 
 	# hole = 24
 	# b4 = ZeroPadding2D(padding=(24, 24))(p5)
-	b4 = Conv2D(64, (3, 3), padding='same',dilation_rate=(24, 24), activation='relu', name='fc6_4', data_format=IMAGE_ORDERING)(p5)
+	b4 = Conv2D(10, (3, 3), padding='same',dilation_rate=(24, 24), activation='relu', name='fc6_4', data_format=IMAGE_ORDERING)(p5)
 	b4 = Dropout(0.5)(b4)
-	b4 = Conv2D(64, (1, 1), activation='relu', name='fc7_4', data_format=IMAGE_ORDERING)(b4)
+	b4 = Conv2D(10, (1, 1), activation='relu', name='fc7_4', data_format=IMAGE_ORDERING)(b4)
 	b4 = Dropout(0.5)(b4)
 	b4 = Conv2D(2, (1, 1), activation='relu', name='fc8_voc12_4', data_format=IMAGE_ORDERING)(b4)
 	
--- result_visulize.py
+++ result_visulize.py
@@ -27,6 +27,40 @@ def visulize(img, mask):
     img.putdata(l)
     img.save('result/'+save_name+'.png')
 
+def visulize1(img, mask):
+    save_name = mask.split('/')[1].split('.')[0]
+    img = Image.open(img)
+    mask = Image.open(mask)
+    
+    shape = np.array(mask).shape
+    img = np.array(img)[0:1080,400:1480]
+    img_shape = img.shape
+    img = Image.fromarray(img)
+    print(shape)
+    img = img.resize((shape[0],shape[0]))
+    print(np.array(mask).shape,np.array(img).shape)
+    # exit(0)
+    # print(shape)
+    # mask = Image.open(mask).resize((shape[0],shape[0]))
+    mask = np.array(mask)[:,:,0]
+    mask[mask < 250] = 0
+    mask[mask >= 250] = 1
+    # mask = np.lib.pad(mask, ((0,0),(400,shape[1]-shape[0]-400)), 'constant', constant_values=(0, 0))
+    mask = Image.fromarray(mask)
+    mask_data = mask.getdata()
+    a = img.getdata()
+    img = img.convert('RGBA')
+    l = list()
+    for i,j in zip(mask_data,a):
+        # print(i)
+        if i == 1:
+            l.append(( 255, j[1], j[2], 150))
+        else:
+            l.append((j[0], j[1], j[2], 255))
+    img.putdata(l)
+    img = img.resize((img_shape[0], img_shape[1]))
+    img.save('result/'+save_name+'.png')
+
 def main():
     imgs_folder = 'new_sheep_val_image/'
     mask_folder = 'predict_target/'
@@ -36,10 +70,11 @@ def main():
     masks.sort()
     assert(len(imgs) == len(masks))
     for img,mask in zip(imgs,masks):
+        print(img)
         visulize(imgs_folder+img, mask_folder+mask)
         # exit(0)
 
-    # visulize('new_sheep_val_image/03.jpg','new_sheep_val_seg/03.png')
+    # visulize1('new_sheep_val_image/03.jpg','predict_target/03.jpg')
     # transPNG('new_sheep_val_seg/03.png','1.png')
 
 if __name__ == '__main__':
